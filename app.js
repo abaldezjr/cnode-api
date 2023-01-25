ac2c1c6528a652a11d7c0988863b10560448988d
Baldez Jr <abaldezjr@gmail.com>
2023-01-25 14:17:57 -0300

    Resposta JSON

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res, next) => {
+//forma de ler JSON (Middleware)
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+);
+
+app.use(express.json());
+
+/*app.use((req, res, next) => {
     res.status(200).send({
-        mensagem:"Deu certo"
+        mensagem: "Deu certo"
     });
+});*/
+
+//rota inicial / endpoint
+app.get('/', (req, res) => {
+    //mostrar req
+
+    res.json({
+        message: 'Oi Express!'
+    });
+
 });
+
+
+//entregar uma porta
+
+
 
 module.exports = app;
